47bc037dea33183adf645cf701f3649efa057381
gihyeon-kim <gihyeonkim@tmapmobility.com>
2026-06-16 11:08:17 +0900

    커버 레이아웃: 상단/중앙/하단 3분할 배치 (샘플 동일)

--- src/component/cover/index.tsx
+++ src/component/cover/index.tsx
@@ -7,26 +7,23 @@ import {
 } from "../../const"
 import { LazyDiv } from "../lazyDiv"
 
-/**
- * 초대장의 메인 커버 섹션입니다.
- * 텍스트만으로 구성된 다크 네이비 + 골드 스타일.
- *
- * @returns {JSX.Element} 커버 섹션
- */
 export const Cover = () => {
   return (
     <LazyDiv className="card cover">
-      <div className="subtitle">Wedding Invitation</div>
-      <div className="sub-desc">We're getting married</div>
-
-      <div className="names">
-        <span>{GROOM_FULLNAME}</span>
-        <span className="and">and</span>
-        <span>{BRIDE_FULLNAME}</span>
+      <div className="header">
+        <div className="title">Wedding Invitation</div>
+        <div className="sub-title">We're getting married</div>
       </div>
 
-      <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
-      <div className="info">{LOCATION}</div>
+      <div className="center">
+        <div className="names">
+          <span>{GROOM_FULLNAME}</span>
+          <span className="and">and</span>
+          <span>{BRIDE_FULLNAME}</span>
+        </div>
+        <div className="info">{WEDDING_DATE.format(WEDDING_DATE_FORMAT)}</div>
+        <div className="info">{LOCATION}</div>
+      </div>
 
       <div className="footer-text">Wedding invitation letter</div>
     </LazyDiv>
