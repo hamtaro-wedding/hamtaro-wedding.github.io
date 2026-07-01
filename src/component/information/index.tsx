dee2358c9801725ad1afb4c811a1693c055c6922
hamtaro-wedding <112359857+hamtaro-wedding@users.noreply.github.com>
2026-07-01 12:25:17 +0900

    복사문구수정

--- src/component/information/index.tsx
+++ src/component/information/index.tsx
@@ -31,7 +31,7 @@ const AccountAccordion = ({
     const numberOnly = account.replace(/[^0-9]/g, "")
     try {
       await navigator.clipboard.writeText(numberOnly)
-      setToast(`${title.replace(" 계좌번호 ", "")} ${name}님의 계좌번호 ${numberOnly}가 복사되었습니다`)
+      setToast(`${title.replace(" 계좌번호", "")} ${name}님의 계좌번호가 복사되었습니다`)
       setTimeout(() => setToast(""), 2000)
     } catch {
       setToast("복사에 실패했습니다")
