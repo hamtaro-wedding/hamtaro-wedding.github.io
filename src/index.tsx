e5f1b57e43e48a057ce7bb574b8594944bded4a1
hamtaro-wedding <112359857+hamtaro-wedding@users.noreply.github.com>
2026-07-01 13:12:05 +0900

    첫페이지 스크롤 버그

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import App from "./App"
 import { ModalProvider } from "./component/modal"
 
 // 애플리케이션의 루트 요소를 가져와서 렌더링을 시작합니다.
+document.documentElement.sytle.setProperty('--inital-vh', '${window.innerHeight * 0.01}px')
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
